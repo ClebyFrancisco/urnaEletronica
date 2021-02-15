dd1e4854169e29fbf429e01960effae7f802bf3c
ClebyFrancisco <clebysilva5@gmail.com>
2021-02-15 01:52:41 -0300

    interação funcionando

---
+++ etapas.js
@@ -0,0 +1,61 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'FULANO DE TAL',
+                partido: 'ABC',
+                fotos: [
+                    {url:'festa-do-milho.png', legenda:' vereador'}
+                ]
+
+            },
+            {
+                numero: '77222',
+                nome: 'BELTRANO DA SILVA',
+                partido: 'DEF',
+                fotos: [
+                    {url:'pascoa.png', legenda:' vereador'}
+                ]
+
+            },
+            {
+                numero: '15000',
+                nome: 'CICLAANO',
+                partido: 'DGH',
+                fotos: [
+                    {url:'reveillon.png', legenda:' vereador'}
+                ]
+
+            },
+            
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 13,
+                nome: 'HADDAD',
+                partido: 'ABC',
+                fotos: [
+                    {url:'38111', legenda:' PREFEITO'}
+                ]
+
+            },
+            {
+                numero: 17,
+                nome: 'BOLSONARO',
+                partido: 'DEF',
+                fotos: [
+                    {url:'17', legenda:' PREFEITO'}
+                ]
+
+            },
+            
+        ]
+    }
+]
